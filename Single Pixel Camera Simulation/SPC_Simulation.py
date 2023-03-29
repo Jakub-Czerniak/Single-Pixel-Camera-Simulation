1e59ec1aec80a03f27d3310f28b3c93dfdef8239
Kuba <Kuba@DESKTOP-FGR0SFK>
2023-03-29 18:24:46 +0200

    Changed from class to script. Added creating of hadamart matrix and loading of images.

--- Single Pixel Camera Simulation/SPC_Simulation.py
+++ Single Pixel Camera Simulation/SPC_Simulation.py
@@ -1,7 +1,62 @@
-from .SinglePixelCamera import SinglePixelCamera
+from PIL import Image
+import sys
+import numpy as np
 
 
-#image = Load Image
-#SinglePixelCamera.Aqusition(image, 100, "Random")
+def Aqusition(image, samplePercent, mode):
+    if(image.size[0]==image.size[1]):
+        px = int(image.size[0] * samplePercent / 100)
+    else:
+        print("Height and width of image does not match.")
+        sys.Exit()
+    image = image.resize((px,px))
+    if(mode=="Hadamard"):
+        hadamard = MakeHadamard(px)
+        hadamardPlus = (hadamard+1)/2
+        hadamardMinus = (1-hadamard)/2
+        print (hadamardMinus)
+        print (hadamardPlus)
+
+    elif(mode=="Random"):
+        print("Random")
+    else:
+        print("Error: Avaible modes: Hadamard, Random")
+        sys.Exit()
+
+
+def AddNoise():
+    print("Not implemented")
+    sys.Exit()
+
+
+def Recovery():
+    print("Not implemented")
+    sys.Exit()
+
+
+def PlotImage():
+    print("Not implemented")
+    sys.Exit()
+
+def MakeHadamard(px):
+    hadamard = np.zeros((px,px))
+    hadamard[0][0] = 1 
+    p = 1
+    while(p<px):
+        for i in range(p):
+            for j in range(p):
+                if(i+p<px):
+                    hadamard[i+p][j]=hadamard[i][j]
+                if(j+p<px):
+                    hadamard[i][j+p]=hadamard[i][j]
+                if(j+p<px and i+p<px):
+                    hadamard[i+p][j+p]=-hadamard[i][p]
+        p*=2
+    return hadamard
+
+image = Image.open("images\photos\Kansas-City.png")
+Aqusition(image=image, samplePercent=10, mode="Hadamard")
+
+
 #SinglePixelCamera.Recovery()
 #SinglePixelCamera.PlotImage()
